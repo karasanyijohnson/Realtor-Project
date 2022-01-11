a6d0f4ed680fc1bb5631d35d0c16823b8733b3d3
Johnson <johnsonkarasanyi@gmail.com>
2022-01-11 09:18:49 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         Our Realtor Project 
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
