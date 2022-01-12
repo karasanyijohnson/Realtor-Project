c8931b1b0de88265dd9e1483be13982346696cb6
Johnson <johnsonkarasanyi@gmail.com>
2022-01-12 09:42:34 +0200

    Start working on Login

--- src/App.js
+++ src/App.js
@@ -1,13 +1,41 @@
+import React from 'react';
+import HomePage from './Screens/AppScreens/Home';
+import Login from './Screens/Authentication/Login';
+import Dashboard from './Screens/AppScreens/Dashboard';
+import './App.css'
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+
+
+// Function to set Token
+const setToken=(userToken)=>{
+sessionStorage.getItem('token', JSON.stringify(userToken));
+}
+// function to get token
+const getToken=()=>{
+
+}
+
 
 function App() {
+  const token = getToken() 
+  // const [token, setToken]=useState() 
+  if(!token){
+    return <Login setToken={setToken}/>
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-         Our Realtor Project 
-        </p>
-      </header>
+    <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/dashboard" element={<Dashboard/>}>
+            </Route>
+          <Route exact path="/" element={<HomePage/>}>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
+
   );
 }
 
